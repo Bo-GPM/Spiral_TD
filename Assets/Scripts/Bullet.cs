15e1d2001a0126be5ac0e72de1c9cc0d1d6ead67
zhuzhu <987582430@qq.com>
2024-01-30 00:10:30 -0800

    Tower rotation & bullet

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    Transform target;
+    [Header("References")]
+    [SerializeField] Rigidbody2D rb;
+
+    [Header("Attributes")]
+    [SerializeField] float bulletSpeed = 5f;
+    [SerializeField] int bulletDamage = 1;
+
+    public void SetTarget(Transform givenEnemy)
+    {
+        //set target inside Tower Script
+        target = givenEnemy;
+    }
+    private void FixedUpdate()
+    {
+        //bullet can follow the target
+        if (!target)
+        {
+            return;
+        }
+        Vector2 direction = (target.position - transform.position).normalized;
+        rb.velocity = direction * bulletSpeed;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //TODO:Damage
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] Transform towerRotatePoint;
+    [SerializeField] LayerMask enemyMask;
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] Transform firePoint;
+
+    [Header("Attribute")]
+    [SerializeField] float range = 5f;
+    [SerializeField] float shootSpeed = 1f;
+
+    Transform target;
+    float timeUntilFire;
+    private void Update()
+    {
+        if (target == null)
+        {
+            FindTarget();
+            return;
+        }
+        RotateTowardsTarget();
+        if (!CheckTargetIsInRange())
+        {
+            target = null;
+        }
+        else 
+        {
+            timeUntilFire += Time.deltaTime;
+            if (timeUntilFire >= 1f / shootSpeed)
+            {
+                Shoot();
+                timeUntilFire = 0f;
+            }
+        }
+    }
+
+    private void Shoot()
+    {
+        GameObject bulletobj=Instantiate(bulletPrefab,firePoint.position,Quaternion.identity);
+        Bullet bulletScript=bulletobj.GetComponent<Bullet>();
+        bulletScript.SetTarget(target);
+        Debug.Log("Shoot");
+    }
+
+    private void FindTarget()
+    {
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, range, (Vector2)transform.position, 0f, enemyMask);
+        if (hits.Length > 0)
+        {
+            target = hits[0].transform;
+            print(target.name);
+        }
+
+    }
+    private void RotateTowardsTarget()
+    {
+        float angle = Mathf.Atan2(target.position.y - transform.position.y, target.position.x - transform.position.x) * Mathf.Rad2Deg;
+        towerRotatePoint.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+    }
+    private bool CheckTargetIsInRange()
+    {
+        return Vector2.Distance(transform.position, target.position) <= range;
+    }
+    private void OnDrawGizmosSelected()
+    {
+        //Range
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, range);
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, range, (Vector2)transform.position, 0f, enemyMask);
+        Gizmos.color = Color.red;
+        foreach (RaycastHit2D hit in hits)
+        {
+            //Hit target
+            Gizmos.DrawLine(transform.position, hit.point);
+            Gizmos.DrawWireSphere(hit.point, 0.2f);
+        }
+
+    }
+}
